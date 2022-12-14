25e7b1e31dff2ced6d58994c37fba1f272ff8f53
may-wagih <86538171+may-wagih@users.noreply.github.com>
2022-12-14 04:15:39 +0200

    added filerted image functionto server.ts file version2

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
   //! END @TODO1
 
-  app.get('/filteredimag',async (req: Request,res:Response) => {
+  app.get('/filteredimage',async (req: Request,res:Response) => {
     const image_url=req.query.image_url.toString();
     if(!image_url){
       res.status(400).send('Image url is required');
@@ -38,7 +38,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     const filtered_image =await filterImageFromURL(image_url);
     res.status(200).sendFile(filtered_image,() => {
       deleteLocalFiles([filtered_image]);
-    });
+    })
   });
 
 
